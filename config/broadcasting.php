dfffbdb5d5fbd2f1d77335e044df7abc51d489b9
Nafies Luthfi <nafiesl@gmail.com>
2020-07-18 14:51:15 +0800

    Update broadcasting config

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -31,18 +31,21 @@ return [
     'connections' => [
 
         'pusher' => [
-            'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
+            'driver'  => 'pusher',
+            'key'     => env('PUSHER_APP_KEY'),
+            'secret'  => env('PUSHER_APP_SECRET'),
+            'app_id'  => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
+                'cluster'   => env('PUSHER_APP_CLUSTER'),
+                'encrypted' => false,
+                'host'      => '127.0.0.1',
+                'port'      => 6001,
+                'scheme'    => 'http',
             ],
         ],
 
         'redis' => [
-            'driver' => 'redis',
+            'driver'     => 'redis',
             'connection' => 'default',
         ],
 
